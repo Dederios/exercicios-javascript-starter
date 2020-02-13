857f7eaeb9aab522a5a1983eebabf9ed9d761a46
Daniel Rios <49802471+Dederios@users.noreply.github.com>
2020-02-13 15:03:19 -0300

    Save in storage

--- modulo3/todos.js
+++ modulo3/todos.js
@@ -2,11 +2,7 @@ var listElement = document.querySelector("#app ul");
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
-    'Fazer café',
-    'Estudar Javascript',
-    'Acessar Comunidade da Rocketseat'
-];
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
 function renderTodos() {
     listElement.innerHTML = '';
@@ -16,13 +12,13 @@ function renderTodos() {
         var todoText = document.createTextNode(todo);
 
         var linkElement = document.createElement('a');
-      
+
         linkElement.setAttribute('href', '#');
 
         var pos = todos.indexOf(todo);
         linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
 
-        var linkText = document.createTextNode('Excluir');
+        var linkText = document.createTextNode(' Excluir');
 
         linkElement.appendChild(linkText);
 
@@ -36,11 +32,12 @@ function renderTodos() {
 renderTodos();
 
 function addTodo() {
-    var todoText = inputElement.value;
+    var todoText = inputElement. value;
 
     todos.push(todoText);
     inputElement.value = '';
     renderTodos();
+    saveToStorage();
 }
 
 buttonElement.onclick = addTodo;
@@ -48,4 +45,9 @@ buttonElement.onclick = addTodo;
 function deleteTodo(pos) {
     todos.splice(pos, 1);
     renderTodos();
+    saveToStorage();
+}
+
+function saveToStorage() {
+    localStorage.setItem('list_todos', JSON.stringify(todos));
 }
